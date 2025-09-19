578e5181bcc6e269b0930c8833a5605c58dfd7ae
MarcoParola <marcoparola96@gmail.com>
2025-09-19 13:52:44 +0200

    add tiny imagenet dataset from hf

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import numpy as np
+from datasets import load_dataset as load_hf_dataset
 
 
 class ImgDataset(torch.utils.data.Dataset):
@@ -52,6 +53,33 @@ def load_dataset(dataset, data_dir, img_size=None, val_split=0.15, test_split=0.
         split = int(len(train) * val_split)
         train, val = torch.utils.data.random_split(train, [len(train) - split, split])
 
+    # TINY IMAGENET
+    elif dataset == 'tinyimagenet':
+        train = load_hf_dataset("zh-plus/tiny-imagenet", split="train")
+        test = load_hf_dataset("zh-plus/tiny-imagenet", split="valid")
+        split = int(len(train) * val_split)
+        train, val = torch.utils.data.random_split(train, [len(train) - split, split])
+        
+        # transform the dataset to apply the transform
+        class HFDatasetWrapper(torch.utils.data.Dataset):
+            def __init__(self, hf_dataset, transform=None):
+                self.hf_dataset = hf_dataset
+                self.transform = transform
+
+            def __len__(self):
+                return len(self.hf_dataset)
+
+            def __getitem__(self, idx):
+                item = self.hf_dataset[idx]
+                img, lbl = item['image'], item['label']
+                if self.transform:
+                    img = self.transform(img)
+                return img, lbl
+
+        train = HFDatasetWrapper(train, transform=transform)
+        val = HFDatasetWrapper(val, transform=transform)
+        test = HFDatasetWrapper(test, transform=transform)
+
     else:
         raise ValueError(f'Unknown dataset: {dataset}')
 
@@ -64,11 +92,23 @@ def load_dataset(dataset, data_dir, img_size=None, val_split=0.15, test_split=0.
 
 if __name__ == "__main__":
     # Example usage
-    dataset_name = 'cifar10'
+    dataset_name = 'tinyimagenet'
     data_dir = './data'
-    train, val, test = load_dataset(dataset_name, data_dir, img_size=32)
+    train, val, test = load_dataset(dataset_name, data_dir)
     print(f'{dataset_name} Train size: {len(train)}, Val size: {len(val)}, Test size: {len(test)}')
 
+    img, lbl = train.__getitem__(0)
+    print(f'Image shape: {img.shape}, Label: {lbl}', 'max:', img.max(), 'min:', img.min())
+
+    from matplotlib import pyplot as plt
+    for i in range(5):
+        img, lbl = train[i]
+        plt.imshow(img.permute(1, 2, 0).numpy())
+        plt.title(f'Label: {lbl}')
+        plt.show()
+
+    '''
     dataset_name = 'cifar100'
     train, val, test = load_dataset(dataset_name, data_dir, img_size=32)
     print(f'{dataset_name} Train size: {len(train)}, Val size: {len(val)}, Test size: {len(test)}')
+    '''
