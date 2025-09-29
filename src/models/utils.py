1c3f2aa35ad21de094e29e516cc76a917139392b
Christian Petruzzella <135434850+christianp12@users.noreply.github.com>
2025-09-29 18:18:17 +0200

    updated src/models/utils.py to load pretrained ViT models and ResNet18 for tinyimagenet

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,7 +1,62 @@
 import timm
 import detectors
 import torch
-from huggingface_hub import hf_hub_download
+import torch.nn as nn
+
+from torchvision.models.resnet import ResNet, BasicBlock
+class ResNet18(ResNet):
+    def __init__(
+        self,
+        channels_in=3,
+        out_dim=10,
+        init_type="kaiming_uniform",
+    ):
+        # call init from parent class
+        super().__init__(block=BasicBlock, layers=[2, 2, 2, 2], num_classes=out_dim)
+        # adpat first layer to fit dimensions
+        self.conv1 = nn.Conv2d(
+            channels_in,
+            64,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False,
+        )
+        self.maxpool = nn.Identity()
+
+        if init_type is not None:
+            self.initialize_weights(init_type)
+
+    def initialize_weights(self, init_type):
+        """
+        applies initialization method on all layers in the network
+        """
+        for m in self.modules():
+            m = self.init_single(init_type, m)
+
+    def init_single(self, init_type, m):
+        """
+        applies initialization method on module object
+        """
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            if init_type == "xavier_uniform":
+                torch.nn.init.xavier_uniform_(m.weight)
+            if init_type == "xavier_normal":
+                torch.nn.init.xavier_normal_(m.weight)
+            if init_type == "uniform":
+                torch.nn.init.uniform_(m.weight)
+            if init_type == "normal":
+                torch.nn.init.normal_(m.weight)
+            if init_type == "kaiming_normal":
+                torch.nn.init.kaiming_normal_(m.weight)
+            if init_type == "kaiming_uniform":
+                torch.nn.init.kaiming_uniform_(m.weight)
+            # set bias to some small non-zero value
+            try:
+                m.bias.data.fill_(0.01)
+            except Exception as e:
+                pass
+        return m
 
 
 def load_model(model_name: str, dataset_name: str):
@@ -21,19 +76,13 @@ def load_model(model_name: str, dataset_name: str):
             model = timm.create_model("resnet18_cifar10", pretrained=True)
         elif dataset_name == 'cifar100':
             model = timm.create_model("resnet18_cifar100", pretrained=True)
+        elif dataset_name == 'tinyimagenet':
+            model = ResNet18(out_dim=200, dropout=0.0)
     elif model_name == 'vit':
         if dataset_name == 'cifar10':
-            checkpoint = hf_hub_download(repo_id="nateraw/vit-base-patch16-224-cifar10", filename="pytorch_model.bin", repo_type="model")
-            model = timm.create_model("vit_base_patch16_224", num_classes=10, pretrained=False)
-            # Load the state dict with strict=False to handle key mismatches
-            state_dict = torch.load(checkpoint, map_location='cpu', weights_only=False)
-            model.load_state_dict(state_dict, strict=False)
+            model = timm.create_model("vit_base_patch16_224_in21k_ft_cifar10", pretrained=True)
         elif dataset_name == 'cifar100':
-            checkpoint = hf_hub_download(repo_id="Ahmed9275/Vit-Cifar100", filename="pytorch_model.bin", repo_type="model")
-            model = timm.create_model("vit_base_patch16_224", num_classes=100, pretrained=False)
-            # Load the state dict with strict=False to handle key mismatches
-            state_dict = torch.load(checkpoint, map_location='cpu', weights_only=False)
-            model.load_state_dict(state_dict, strict=False)
+            model = timm.create_model("vit_base_patch16_224_in21k_ft_cifar100", pretrained = True)
 
     return model
 
