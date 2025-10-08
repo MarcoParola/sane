f7d5100e6fa625a2b0e21f671dde3ff94cc5c4f1
Christian Petruzzella <135434850+christianp12@users.noreply.github.com>
2025-10-08 23:28:42 +0200

    added test_sane (works for zoo only at the moment)

---
+++ test_sane.py
@@ -0,0 +1,140 @@
+import torch
+from src.models.sane.sane import Sane
+import hydra
+from lightning.pytorch import Trainer
+from src.utils.log import get_loggers
+from src.datasets.weights.tokenized_model_weights import TokenizedZooDataset, TokenizedModelWeightDataset
+from src.utils.tokenizer import Tokenizer
+from pathlib import Path
+from test_classifier import test_classifier 
+from src.utils.plots import layers_histogram
+from src.datasets.utils import load_dataset
+from src.models.utils import load_model
+from src.utils.log import get_loggers
+from wandb import Image as WBImage
+import wandb
+from lightning.pytorch.loggers import WandbLogger
+
+
+def reconstruct_and_test_model(cfg, original_checkpoint, tokenizer, trainer, sane_model, classifier_network, test, n_classes, batch_size, device, remapping, counter, original_metrics):
+    testset = TokenizedModelWeightDataset(original_checkpoint, tokenizer, cfg.transformer.blocksize)
+    testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=cfg.training.batch_size, shuffle=False, num_workers=cfg.training.num_workers, persistent_workers=True)
+    trainer.test(sane_model, dataloaders=testloader)
+    recontokens, positions, embeddings = sane_model.get_test_outputs()
+    injected_checkpoint = tokenizer.detokenize(recontokens, positions, original_checkpoint)
+    injected_checkpoint_location = Path(f"checkpoints/injections/single_model_trained/resnet18_tinyimagenet")
+    injected_checkpoint_location.mkdir(777, parents=True, exist_ok=True)
+    injected_checkpoint_location = injected_checkpoint_location.joinpath(f"injected_{counter}.pt")
+    injected_checkpoint_location.unlink(missing_ok=True); torch.save(injected_checkpoint, injected_checkpoint_location)
+    injected_checkpoint = torch.load(injected_checkpoint_location, weights_only=False)
+
+    # classification task
+    print(f"Injected Model {counter}:")
+    classifier_network.load_state_dict(injected_checkpoint)
+    classifier_network.eval()
+    injected_metrics = test_classifier(classifier_network, test, n_classes, batch_size, device, remapping)
+
+    # layer by layer histogram plotting
+    if trainer.logger:
+        print("\nLogging...")
+        wandb_run = trainer.logger.experiment
+        #for idx, layer, figure, mse in layers_histogram(original_checkpoint, injected_checkpoint):
+        #    if idx != -1: 
+        #        wandb_run.log({f"{idx}.{layer}/plot": WBImage(figure), f"MSEs/{idx}.{layer}": mse})
+        #    else: wandb_run.log({f"Test/{layer}": WBImage(figure)}) # layer becomes the plot's title
+        
+        wandb_run.log({f"Test/Original_{k}": v[0] for k,v in original_metrics.todict().items()})
+        wandb_run.log({f"Test/Injected_{k}": v[0] for k,v in injected_metrics.todict().items()})
+        wandb_run.finish()
+
+
+@hydra.main(config_path="config", config_name="config", version_base=None)
+def main(cfg):
+    stride = cfg.transformer.blocksize // cfg.training.stride
+    log_run = f"test_sane_{cfg.experiment.mode}.test_sws{cfg.training.stride}_b{cfg.transformer.n_blocks}_e{cfg.transformer.edim}_h{cfg.transformer.n_head}"
+    run_group, run_name = log_run.split(".")
+    callbacks = list()
+    loggers = get_loggers(cfg, run_group, run_name)
+
+    print("Loading Sane architecture...")
+    sane_model = Sane(
+        conf = cfg,
+        idim = cfg.transformer.blocksize,
+        edim = cfg.transformer.edim,
+        n_head=cfg.transformer.n_head,
+        n_blocks=cfg.transformer.n_blocks,
+        wsize=cfg.transformer.blocksize
+    )
+
+    trainer = Trainer(
+        max_epochs = cfg.training.n_epochs,
+        callbacks = callbacks,
+        logger = loggers
+    )
+
+    print("Loading pretrained Sane weights...")
+    sane_checkpoint_path = "checkpoints/sane_model/sws4_b2_e256_h2/best/zoo_50_models_sane-epoch=960-val_loss=0.0000.ckpt"
+    sane_checkpoint = torch.load(sane_checkpoint_path, map_location="cpu", weights_only=False)
+    sane_model.load_state_dict(sane_checkpoint['state_dict'])
+
+    print("\nLoading Tokenized Zoo test set...")
+    tokenizer = Tokenizer(cfg.transformer.blocksize)
+    zoo_models_path = []
+    tinyimagenet_path = "checkpoints/tiny-imagenet_resnet18_kaiming_uniform_subset"
+    zoo_models_path.append(tinyimagenet_path)
+
+    if cfg.test.test_error:
+        print("\nTesting Sane model...")
+        dataset = TokenizedZooDataset(zoo_models_path, tokenizer, cfg.transformer.blocksize, stride=stride)
+        train_set, val_set, test_set = torch.utils.data.random_split(dataset, [int(0.7*len(dataset)), int(0.15*len(dataset)), len(dataset) - int(0.7*len(dataset)) - int(0.15*len(dataset))])
+        testloader = torch.utils.data.DataLoader(dataset=test_set, batch_size=cfg.training.batch_size, shuffle=False, num_workers=0, persistent_workers=False)
+        trainer.test(sane_model, dataloaders=testloader)
+
+    if cfg.test.reconstruction_error:
+        # classification task preparation
+        model_name = cfg.model.name
+        dataset_name = cfg.dataset.name
+        n_classes = cfg[dataset_name].num_classes
+        if (model_name == "vit" and dataset_name == "cifar10"):
+            img_size = 224
+        else:
+            img_size = cfg[dataset_name].img_size
+        batch_size = 32 if model_name == "vit" else cfg.training.batch_size
+        device = cfg.training.device
+        data_dir = cfg.data_dir
+
+        train, val, test, remapping = load_dataset(dataset_name, data_dir, img_size)
+
+        print(f"\nModel: {model_name} \nDataset: {dataset_name} \nNum Classes: {n_classes} \nImage Size: {img_size} \nBatch Size: {batch_size}")
+        classifier_network = load_model(model_name, dataset_name).to(device)
+
+        print("\nTesting reconstruction and predictions")
+        counter = -1
+        for zoo in zoo_models_path:
+            zoo_path = Path(zoo)
+            for folder in zoo_path.iterdir():
+                if folder.is_dir():
+                    counter += 1
+                    if 0 <= counter <= 25:
+                        current_checkpoint_path = folder / "checkpoint_000060/checkpoints"
+                        if current_checkpoint_path.exists():
+                            wandb.finish()  # ensure previous run is closed
+                            wandb_logger = WandbLogger(project="test_sane", name=f"model_{counter}")
+                            trainer = Trainer(logger=wandb_logger)
+                            checkpoint = torch.load(current_checkpoint_path, weights_only=False)
+                            
+                            # test original model only once
+                            if counter == 0:
+                                print("\nOriginal Model:")
+                                classifier_network.load_state_dict(checkpoint)
+                                classifier_network.eval()
+                                original_metrics = test_classifier(classifier_network, test, n_classes, batch_size, device, remapping)
+
+                            print(f"\nReconstructing model {counter}")
+                            reconstruct_and_test_model(cfg, checkpoint, tokenizer, trainer, sane_model, classifier_network, test, n_classes, batch_size, device, remapping, counter, original_metrics)
+                    elif counter > 8:
+                        break
+
+
+if __name__ == "__main__":
+    main()
