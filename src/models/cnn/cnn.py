10422fe889837b2702155f5fbd502c721c9eea40
Christian Petruzzella <135434850+christianp12@users.noreply.github.com>
2025-11-05 13:36:27 +0100

    implemented large cnn architecture

--- src/models/cnn/cnn.py
+++ src/models/cnn/cnn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class CNN(nn.Module):
+class SmallCNN(nn.Module):
     def __init__(
         self,
         channels_in=3,
@@ -37,6 +37,114 @@ class CNN(nn.Module):
         self.module_list.append(self.get_nonlin(nlin))
         ## add dropout
         if dropout > 0:
+            self.module_list.append(nn.Dropout(dropout))
+        ## add linear layer 1
+        self.module_list.append(nn.Linear(20, 10))
+
+        ### initialize weights with se methods
+        self.initialize_weights(init_type)
+
+    def initialize_weights(self, init_type):
+        # print("initialze model")
+        for m in self.module_list:
+            if type(m) == nn.Linear or type(m) == nn.Conv2d:
+                if init_type == "xavier_uniform":
+                    torch.nn.init.xavier_uniform_(m.weight)
+                if init_type == "xavier_normal":
+                    torch.nn.init.xavier_normal_(m.weight)
+                if init_type == "uniform":
+                    torch.nn.init.uniform_(m.weight)
+                if init_type == "normal":
+                    torch.nn.init.normal_(m.weight)
+                if init_type == "kaiming_normal":
+                    torch.nn.init.kaiming_normal_(m.weight)
+                if init_type == "kaiming_uniform":
+                    torch.nn.init.kaiming_uniform_(m.weight)
+                # set bias to some small non-zero value
+                m.bias.data.fill_(0.01)
+
+    def get_nonlin(self, nlin):
+        # apply nonlinearity
+        if nlin == "leakyrelu":
+            return nn.LeakyReLU()
+        if nlin == "relu":
+            return nn.ReLU()
+        if nlin == "tanh":
+            return nn.Tanh()
+        if nlin == "sigmoid":
+            return nn.Sigmoid()
+        if nlin == "silu":
+            return nn.SiLU()
+        if nlin == "gelu":
+            return nn.GELU()
+
+    def forward(self, x):
+        # forward prop through module_list
+        for layer in self.module_list:
+            x = layer(x)
+        return x
+
+    def forward_activations(self, x):
+        # forward prop through module_list
+        activations = []
+        for layer in self.module_list:
+            x = layer(x)
+            if (
+                isinstance(layer, nn.Tanh)
+                or isinstance(layer, nn.Sigmoid)
+                or isinstance(layer, nn.ReLU)
+                or isinstance(layer, nn.LeakyReLU)
+                or isinstance(layer, nn.LeakyReLU)
+                or isinstance(layer, nn.SiLU)
+                or isinstance(layer, nn.GELU)
+            ):
+                activations.append(x)
+        return x, activations
+    
+
+class LargeCNN(nn.Module):
+    def __init__(
+        self,
+        channels_in,
+        nlin="leakyrelu",
+        dropout=0.2,
+        init_type="uniform",
+    ):
+        super().__init__()
+        # init module list
+        self.module_list = nn.ModuleList()
+        ### ASSUMES 32x32 image size
+        ## chn_in * 32 * 32
+        ## compose layer 0
+        self.module_list.append(nn.Conv2d(channels_in, 16, 3))
+        self.module_list.append(nn.MaxPool2d(2, 2))
+        self.module_list.append(self.get_nonlin(nlin))
+        # apply dropout
+        if True:  # dropout > 0:
+            self.module_list.append(nn.Dropout(dropout))
+        ## 16 * 15 * 15
+        ## compose layer 1
+        self.module_list.append(nn.Conv2d(16, 32, 3))
+        self.module_list.append(nn.MaxPool2d(2, 2))
+        self.module_list.append(self.get_nonlin(nlin))
+        # apply dropout
+        if True:  # dropout > 0:
+            self.module_list.append(nn.Dropout(dropout))
+        ## 32 * 7 * 7 // 32 * 6 * 6
+        ## compose layer 2
+        self.module_list.append(nn.Conv2d(32, 15, 3))
+        self.module_list.append(nn.MaxPool2d(2, 2))
+        self.module_list.append(self.get_nonlin(nlin))
+        ## add dropout
+        if True:  # dropout > 0:
+            self.module_list.append(nn.Dropout(dropout))
+        ## 15 * 2 * 2
+        self.module_list.append(nn.Flatten())
+        ## add linear layer 1
+        self.module_list.append(nn.Linear(15 * 2 * 2, 20))
+        self.module_list.append(self.get_nonlin(nlin))
+        ## add dropout
+        if True:  # dropout > 0:
             self.module_list.append(nn.Dropout(dropout))
         ## add linear layer 1
         self.module_list.append(nn.Linear(20, 10))
