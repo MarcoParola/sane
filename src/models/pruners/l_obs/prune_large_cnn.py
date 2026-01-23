5e47d930fc31b7c136bd4c7e459e4c8cfb10762f
Christian Petruzzella <135434850+christianp12@users.noreply.github.com>
2026-01-23 11:44:15 +0100

    updated l_obs to consider only one sparsity level at a time

--- src/models/pruners/l_obs/prune_large_cnn.py
+++ src/models/pruners/l_obs/prune_large_cnn.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy.linalg import inv, pinv
 from pathlib import Path
 
-def prune_large_cnn(store_location, checkpoint, dataset_name, data_dir, model_name, img_size):
+def prune_large_cnn(store_location, checkpoint, dataset_name, data_dir, model_name, img_size, sparsity_level):
     current_zoo_model_path = Path(*store_location.parts[3:])
     current_path = f"{model_name}_{dataset_name}" / current_zoo_model_path
 
@@ -52,66 +52,64 @@ def prune_large_cnn(store_location, checkpoint, dataset_name, data_dir, model_na
 
     # PRUNE WEIGHTS
     state_dict = checkpoint.state_dict()
-    target_sparsities = [0.3,0.5,0.8]
 
-    for sparsity in target_sparsities:
-        cr_save_dir = Path(f"checkpoints/sparsified_zoos/{model_name}_{dataset_name}_l_obs_sparsity_{sparsity}") / current_zoo_model_path
-        cr_save_dir.mkdir(parents=True, exist_ok=True)
-        pruned_checkpoint_file = cr_save_dir / "checkpoints"
+    cr_save_dir = Path(f"checkpoints/sparsified_zoos/{model_name}_{dataset_name}_l_obs_sparsity_{sparsity_level}") / current_zoo_model_path
+    cr_save_dir.mkdir(parents=True, exist_ok=True)
+    pruned_checkpoint_file = cr_save_dir / "checkpoints"
 
-        # Skip pruning if checkpoint already exists
-        if pruned_checkpoint_file.exists():
-            print(f"Pruned checkpoint for sparsity {sparsity} already exists. Skipping...")
-            continue
+    # Skip pruning if checkpoint already exists
+    #if pruned_checkpoint_file.exists():
+    #    print(f"Pruned checkpoint for sparsity {sparsity} already exists. Skipping...")
+    #    continue
 
-        pruned_state_dict = {}
+    pruned_state_dict = {}
 
-        for layer_name in layer_name_list:
-            if layer_name in ['module_list.0', 'module_list.4', 'module_list.8']:
-                layer_type = 'C'
-                kernel = state_dict[f'{layer_name}.weight'].cpu().numpy()
-                kernel_shape = kernel.shape
-                weight = unfold_kernel(kernel)
-                bias   = state_dict[f'{layer_name}.bias'].cpu().numpy()
-                wb = np.concatenate([weight, bias.reshape(1, -1)], axis = 0)
-            elif layer_name in ['module_list.13', 'module_list.16']:
-                layer_type = 'F'
-                weight = state_dict[f'{layer_name}.weight'].cpu().numpy()
-                bias   = state_dict[f'{layer_name}.bias'].cpu().numpy()
-                wb = np.hstack([weight, bias.reshape(-1, 1)]).transpose()
-        
-            # Load Hessian inverse
-            hessian_inv = np.load(f'{hessian_inverse_root}/{layer_name}.npy')
+    for layer_name in layer_name_list:
+        if layer_name in ['module_list.0', 'module_list.4', 'module_list.8']:
+            layer_type = 'C'
+            kernel = state_dict[f'{layer_name}.weight'].cpu().numpy()
+            kernel_shape = kernel.shape
+            weight = unfold_kernel(kernel)
+            bias   = state_dict[f'{layer_name}.bias'].cpu().numpy()
+            wb = np.concatenate([weight, bias.reshape(1, -1)], axis = 0)
+        elif layer_name in ['module_list.13', 'module_list.16']:
+            layer_type = 'F'
+            weight = state_dict[f'{layer_name}.weight'].cpu().numpy()
+            bias   = state_dict[f'{layer_name}.bias'].cpu().numpy()
+            wb = np.hstack([weight, bias.reshape(-1, 1)]).transpose()
+    
+        # Load Hessian inverse
+        hessian_inv = np.load(f'{hessian_inverse_root}/{layer_name}.npy')
 
-            # Sensitivity ranking
-            diag = np.diag(hessian_inv).reshape(-1, 1)
-            L = (wb**2) / (diag + 1e-5)
-            sen_rank = np.argsort(L.ravel())
+        # Sensitivity ranking
+        diag = np.diag(hessian_inv).reshape(-1, 1)
+        L = (wb**2) / (diag + 1e-5)
+        sen_rank = np.argsort(L.ravel())
 
-            # determine number of weights to prune
-            l1, l2 = wb.shape
-            n_prune = int(l1*l2*sparsity)            
-            mask = np.ones(wb.shape)
-                    
-            # prune weights
-            for prune_idx in sen_rank[:n_prune]:
-                prune_row_idx = prune_idx // l2
-                prune_col_idx = prune_idx % l2
-                delta_W = - wb[prune_row_idx, prune_col_idx] / (hessian_inv[prune_row_idx, prune_row_idx] + 1e-5) * hessian_inv[:, prune_row_idx]
-                wb[:, prune_col_idx] += delta_W
-                mask[prune_row_idx, prune_col_idx] = 0
+        # determine number of weights to prune
+        l1, l2 = wb.shape
+        n_prune = int(l1*l2*sparsity_level)            
+        mask = np.ones(wb.shape)
+                
+        # prune weights
+        for prune_idx in sen_rank[:n_prune]:
+            prune_row_idx = prune_idx // l2
+            prune_col_idx = prune_idx % l2
+            delta_W = - wb[prune_row_idx, prune_col_idx] / (hessian_inv[prune_row_idx, prune_row_idx] + 1e-5) * hessian_inv[:, prune_row_idx]
+            wb[:, prune_col_idx] += delta_W
+            mask[prune_row_idx, prune_col_idx] = 0
 
-            wb_masked = np.multiply(wb, mask)
+        wb_masked = np.multiply(wb, mask)
 
-            if layer_type == 'F':
-                w = wb_masked[0:-1, :].transpose()
-                b = wb_masked[-1, :].transpose()
-                pruned_state_dict[f"{layer_name}.weight"] = torch.from_numpy(w)
-                pruned_state_dict[f"{layer_name}.bias"] = torch.from_numpy(b)
-            elif layer_type == 'C':
-                kernel_pruned = fold_weights(wb_masked[0:-1, :], kernel_shape)
-                bias_pruned = wb_masked[-1, :]
-                pruned_state_dict[f"{layer_name}.weight"] = torch.from_numpy(kernel_pruned)
-                pruned_state_dict[f"{layer_name}.bias"] = torch.from_numpy(bias_pruned)
+        if layer_type == 'F':
+            w = wb_masked[0:-1, :].transpose()
+            b = wb_masked[-1, :].transpose()
+            pruned_state_dict[f"{layer_name}.weight"] = torch.from_numpy(w)
+            pruned_state_dict[f"{layer_name}.bias"] = torch.from_numpy(b)
+        elif layer_type == 'C':
+            kernel_pruned = fold_weights(wb_masked[0:-1, :], kernel_shape)
+            bias_pruned = wb_masked[-1, :]
+            pruned_state_dict[f"{layer_name}.weight"] = torch.from_numpy(kernel_pruned)
+            pruned_state_dict[f"{layer_name}.bias"] = torch.from_numpy(bias_pruned)
 
-        torch.save(pruned_state_dict, f"{cr_save_dir}/checkpoints")
+    torch.save(pruned_state_dict, f"{cr_save_dir}/checkpoints")
